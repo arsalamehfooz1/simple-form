d2b11b65db0270358f562c7c73976e2989e719d6
arsalamehfooz1 <arsalamehfooz@gmail.com>
2022-06-24 23:33:06 +0500

    simple-form

---
+++ script.js
@@ -0,0 +1,13 @@
+function info() {
+var name=document.getElementById('firstName').value;
+var lastName=document.getElementById('lastName').value;
+var email=document.getElementById('email').value;
+var password=document.getElementById('password').value;
+document.write('Name   '+name+ '<br>')
+document.write('LastName    '+lastname+ '<br>')
+document.write('Email   '+email+ '<br>')
+document.write('password    '+password+ '<br>')
+
+
+
+}
